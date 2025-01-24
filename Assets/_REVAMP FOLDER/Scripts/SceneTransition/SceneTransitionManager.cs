c062a3b47c5cc77361f368aa361eaf5520805fc0
JhanPerez32 <jhankhendrickperez@gmail.com>
2025-01-24 18:27:25 +0800

    MainGameScene Update
    Added:
    -Essentials Prefab (UI's, Character)
    -Teleporters
    -Added the MainGame scene on the SceneTransitionManager
    
    Bug found upon the changes
    -Cinemachine of the City is not following the player
    -Cinemachine of the Apartment is not following the player upon entry from Bedroom

--- Assets/_REVAMP FOLDER/Scripts/SceneTransition/SceneTransitionManager.cs
+++ Assets/_REVAMP FOLDER/Scripts/SceneTransition/SceneTransitionManager.cs
@@ -8,7 +8,7 @@ public class SceneTransitionManager : MonoBehaviour
 {
     public static SceneTransitionManager Instance;
 
-    public enum Location { Bedroom, PlantingArea, TestCity}
+    public enum Location { Bedroom, PlantingArea, TestCity, MainGame}
     public Location currentLocation;
 
     Transform playerPoint;
