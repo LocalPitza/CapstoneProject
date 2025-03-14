3a0ba7136f7bfaf8f3f99867ad1e8da7cfd7f935
JhanPerez32 <jhankhendrickperez@gmail.com>
2025-03-14 18:41:25 +0800

    Update on Food Data Information, auto  info update based on the inputed details

--- Assets/_REVAMP FOLDER/Scripts/Inventory/FoodData.cs
+++ Assets/_REVAMP FOLDER/Scripts/Inventory/FoodData.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Items/Food")]
@@ -11,6 +12,31 @@ public class FoodData : ItemData
     public bool requirePayment;
     public bool needIngredient;
     public RequiredIngredient[] requiredIngredients;
+
+    private void OnValidate()
+    {
+        UpdateDescription();
+    }
+
+    private void UpdateDescription()
+    {
+        // **Reset NewDescription every time OnValidate() runs** to avoid duplication
+        NewDescription = description;
+
+        // Append Hunger and Energy Refill values
+        NewDescription += $"\n\nHunger Refill: {hungerRefill}\nEnergy Refill: {energyRefill}";
+
+        // Append required ingredients if needed
+        if (needIngredient && requiredIngredients.Length > 0)
+        {
+            NewDescription += "\nRequired Ingredients:";
+            foreach (var ingredient in requiredIngredients)
+            {
+                if (ingredient.ingredient != null) // Ensure ingredient is not null
+                    NewDescription += $"\n- {ingredient.ingredient.name} x{ingredient.requiredAmount}";
+            }
+        }
+    }
 
     public void OnConsume()
     {
