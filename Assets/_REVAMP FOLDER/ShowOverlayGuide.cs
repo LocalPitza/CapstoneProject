977fba1143cd616cbc53fc4b69eb2672876887f9
JhanPerez32 <jhankhendrickperez@gmail.com>
2025-03-16 19:39:21 +0800

    Guide Overlay

---
+++ Assets/_REVAMP FOLDER/ShowOverlayGuide.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShowOverlayGuide : MonoBehaviour
+{
+    public GameObject overlayGuide;
+    public TextMeshProUGUI toggleButtonText;
+
+    void Start()
+    {
+        // Ensure the overlay is disabled at start
+        if (overlayGuide != null)
+            overlayGuide.SetActive(false);
+
+        UpdateButtonText();
+    }
+
+    void Update()
+    {
+        // Toggle overlay with F1 key
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            ToggleOverlay();
+        }
+    }
+
+    // Function for button press
+    public void ToggleOverlay()
+    {
+        if (overlayGuide != null)
+        {
+            overlayGuide.SetActive(!overlayGuide.activeSelf);
+            UpdateButtonText();
+        }
+    }
+
+    // Update the button text based on overlay state
+    private void UpdateButtonText()
+    {
+        if (toggleButtonText != null)
+        {
+            toggleButtonText.text = overlayGuide.activeSelf ? "Hide Overlay" : "Show Overlay";
+        }
+    }
+}
